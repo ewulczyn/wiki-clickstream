ece1330ed181f5011d826616d2c90df6cb4fa9a1
Ellery Wulczyn <ellerywulczyn@ElleryWczynsMBP.corp.wikimedia.org>
2016-04-28 17:54:16 -0700

    split query into views

--- src/get_clickstream.py
+++ src/get_clickstream.py
@@ -8,9 +8,9 @@ from sqoop_utils import sqoop_prod_dbs
 Usage:
 
 python get_clickstream.py \
-    --start 2016-03-01 \
-    --stop  2016-03-31 \
-    --table 2016_03 \
+    --start 2016-04-01 \
+    --stop  2016-04-01 \
+    --table test \
     --lang en \
     --priority
 
@@ -25,103 +25,142 @@ def get_clickstream(table, lang, start, stop, priority = False, min_count = 10):
                 }
 
     query = """
-    DROP TABLE IF EXISTS clickstream.%(table)s;
-    CREATE TABLE clickstream.%(table)s
-    ROW FORMAT DELIMITED
-    FIELDS TERMINATED BY '\t'
-    STORED AS TEXTFILE
-    AS SELECT
+
+    -- extract raw prev, curr pairs
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp1;
+    CREATE VIEW clickstream.%(table)s_temp1 AS
+    SELECT 
+        CASE
+            -- empty or malformed referer
+            WHEN referer IS NULL THEN 'other-empty'
+            WHEN referer == '' THEN 'other-empty'
+            WHEN referer == '-' THEN 'other-empty'
+            WHEN parse_url(referer,'HOST') is NULL THEN 'other-empty'
+            -- internal referer from the same wikipedia
+            WHEN 
+                parse_url(referer,'HOST') in ('%(lang)s.wikipedia.org', '%(lang)s.m.wikipedia.org')
+                AND LENGTH(REGEXP_EXTRACT(parse_url(referer,'PATH'), '/wiki/(.*)', 1)) > 1
+            THEN REGEXP_EXTRACT(parse_url(referer,'PATH'), '/wiki/(.*)', 1)
+            -- other referers 
+            WHEN referer_class = 'internal' THEN 'other-internal'
+            WHEN referer_class = 'external' THEN 'other-external'
+            WHEN referer_class = 'external (search engine)' THEN 'other-search'
+            ELSE 'other-other'
+        END as prev,
+        pageview_info['page_title'] as curr
+    FROM
+        wmf.webrequest
+    WHERE 
+        %(time_conditions)s
+        AND hour = 1
+        AND webrequest_source = 'text'
+        AND normalized_host.project_class = 'wikipedia'
+        AND normalized_host.project = '%(lang)s'
+        AND is_pageview 
+        AND agent_type = 'user';
+
+
+    -- count raw prev, curr pairs
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp2;
+    CREATE VIEW clickstream.%(table)s_temp2 AS
+    SELECT
+        curr, prev, COUNT(*) as n
+    FROM
+        clickstream.%(table)s_temp1
+    GROUP BY 
+        curr, prev;
+
+
+    -- resolve redirects
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp3;
+    CREATE VIEW clickstream.%(table)s_temp3 AS
+    SELECT 
+        CASE
+            WHEN prev  in ('other-empty', 'other-internal', 'other-external', 'other-search', 'other-other') THEN prev
+            WHEN pr.rd_to IS NULL THEN prev
+            ELSE pr.rd_to
+        END AS prev,
+        CASE
+            WHEN cr.rd_to IS NULL THEN curr
+            ELSE cr.rd_to
+        END AS curr,
+        n
+    FROM
+        clickstream.%(table)s_temp2
+    LEFT JOIN
+        clickstream.%(lang)s_redirect pr ON (prev = pr.rd_from)
+    LEFT JOIN
+        clickstream.%(lang)s_redirect cr ON (curr = cr.rd_from);
+
+    -- re-aggregate after resolving redirects and filter out pairs that occur infrequently
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp4;
+    CREATE VIEW clickstream.%(table)s_temp4 AS
+    SELECT
+        curr, prev, SUM(n) as n
+    FROM
+        clickstream.%(table)s_temp3
+    GROUP BY
+        curr, prev
+    HAVING
+        SUM(n) > %(min_count)s;
+
+    -- only include main namespace articles
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp5;
+    CREATE VIEW clickstream.%(table)s_temp5 AS
+    SELECT 
+        curr, prev, n
+    FROM
+        clickstream.%(table)s_temp4
+    LEFT JOIN
+        clickstream.%(lang)s_page_raw pp ON (prev = pp.page_title)
+    LEFT JOIN
+        clickstream.%(lang)s_page_raw cp ON (curr = cp.page_title)
+    WHERE
+        cp.page_title is not NULL
+        AND ( pp.page_title is NOT NULL
+              OR prev  in ('other-empty', 'other-internal', 'other-external', 'other-search', 'other-other')
+            );
+
+    -- annotate link types
+    DROP VIEW IF EXISTS clickstream.%(table)s_temp6;
+    CREATE VIEW clickstream.%(table)s_temp6 AS
+    SELECT
         prev,
         curr,
         CASE
-            WHEN prev  in ('other-wikipedia', 'other-google', 'other-yahoo', 'other-empty', 'other-other', 'other-facebook', 'other-twitter', 'other-bing', 'other-internal') THEN 'external'
+            WHEN prev  in ('other-empty', 'other-internal', 'other-external', 'other-search', 'other-other') THEN 'external'
             WHEN l.pl_from IS NOT NULL AND l.pl_to IS NOT NULL THEN 'link'
             ELSE 'other'
         END AS type,
         n
     FROM
-        (SELECT 
-            curr, prev, n
-        FROM
-            (SELECT
-                curr, prev, SUM(n) as n
-            FROM
-                (SELECT 
-                    CASE
-                        WHEN prev  in ('other-wikipedia', 'other-google', 'other-yahoo', 'other-empty', 'other-other', 'other-facebook', 'other-twitter', 'other-bing', 'other-internal') THEN prev
-                        WHEN pr.rd_to IS NULL THEN prev
-                        ELSE pr.rd_to
-                    END AS prev,
-                    CASE
-                        WHEN cr.rd_to IS NULL THEN curr
-                        ELSE cr.rd_to
-                    END AS curr,
-                    n
-                FROM
-                    (SELECT
-                        curr, prev, COUNT(*) as n
-                    FROM
-                        (SELECT 
-                            REGEXP_EXTRACT(reflect('java.net.URLDecoder', 'decode', uri_path), '/wiki/(.*)', 1) as curr,
-                            CASE
-                                WHEN referer == '' THEN 'other-empty'
-                                WHEN referer == '-' THEN 'other-empty'
-                                WHEN referer IS NULL THEN 'other-empty'
-                                WHEN parse_url(referer,'HOST') is NULL THEN 'other-empty'
-                                WHEN parse_url(referer,'HOST') RLIKE 'google.' THEN 'other-google'
-                                WHEN parse_url(referer,'HOST') RLIKE 'yahoo.' THEN 'other-yahoo'
-                                WHEN parse_url(referer,'HOST') RLIKE 'facebook.' THEN 'other-facebook'
-                                WHEN parse_url(referer,'HOST') RLIKE 'twitter.' THEN 'other-twitter'
-                                WHEN parse_url(referer,'HOST') RLIKE 't.co' THEN 'other-twitter'
-                                WHEN parse_url(referer,'HOST') RLIKE 'bing.' THEN 'other-bing'
-                                WHEN 
-                                    parse_url(referer,'HOST') in ('%(lang)s.wikipedia.org', '%(lang)s.m.wikipedia.org')
-                                    AND LENGTH(REGEXP_EXTRACT(parse_url(referer,'PATH'), '/wiki/(.*)', 1)) > 1
-                                THEN REGEXP_EXTRACT(parse_url(referer,'PATH'), '/wiki/(.*)', 1)
-                                WHEN parse_url(referer,'HOST') RLIKE '.wikipedia.org' THEN 'other-wikipedia'
-                                WHEN parse_url(referer,'HOST') RLIKE '\\.wiki.*\\.org' THEN 'other-internal'
-                                ELSE 'other-other'
-                            END as prev
-                            FROM wmf.webrequest
-                        WHERE 
-                            %(time_conditions)s
-                            AND webrequest_source = 'text'
-                            AND normalized_host.project_class = 'wikipedia'
-                            AND normalized_host.project = '%(lang)s'
-                            AND is_pageview 
-                            AND LENGTH(REGEXP_EXTRACT(reflect('java.net.URLDecoder', 'decode', uri_path), '/wiki/(.*)', 1)) > 0
-                            AND agent_type = 'user'
-                        ) pc0 -- extract pageviews and re-map referer
-                    GROUP BY 
-                        curr, prev
-                    ) pc1  -- group by curr, prev
-                LEFT JOIN
-                    clickstream.%(lang)s_redirect pr ON (pc1.prev = pr.rd_from)
-                LEFT JOIN
-                    clickstream.%(lang)s_redirect cr ON (pc1.curr = cr.rd_from)
-                ) pc2 --resolve redirects
-            GROUP BY
-                curr, prev
-            HAVING
-                SUM(n) > %(min_count)s
-            ) pc3 -- re-aggregate and drop pairs under min count
-        LEFT JOIN
-            clickstream.%(lang)s_page_raw pp ON (pc3.prev = pp.page_title)
-        LEFT JOIN
-            clickstream.%(lang)s_page_raw cp ON (pc3.curr = cp.page_title)
-        WHERE
-            cp.page_title is not NULL
-            AND ( pp.page_title is NOT NULL
-                  OR prev  in ('other-wikipedia', 'other-google', 'other-yahoo', 'other-empty', 'other-other', 'other-facebook', 'other-twitter', 'other-bing', 'other-internal')
-                )
-        ) pc4 -- only include main namespace articles
+        clickstream.%(table)s_temp5
     LEFT JOIN
-        clickstream.%(lang)s_pagelinks l ON (pc4.prev = l.pl_from AND pc4.curr = l.pl_to)
+        clickstream.%(lang)s_pagelinks l ON (prev = l.pl_from AND curr = l.pl_to);
+
+
+    -- create table
+    DROP TABLE IF EXISTS clickstream.%(table)s;
+    CREATE TABLE clickstream.%(table)s
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY '\t'
+    STORED AS TEXTFILE AS
+    SELECT
+        *
+    FROM
+        clickstream.%(table)s_temp6
     WHERE 
-        curr != prev -- drop self loops
-    -- annotate link types
+        curr != prev;
+
+    DROP VIEW clickstream.%(table)s_temp1;
+    DROP VIEW clickstream.%(table)s_temp2;
+    DROP VIEW clickstream.%(table)s_temp3;
+    DROP VIEW clickstream.%(table)s_temp4;
+    DROP VIEW clickstream.%(table)s_temp5;
+    DROP VIEW clickstream.%(table)s_temp6;
     """
 
+    print(query % params)
     exec_hive_stat2(query % params, priority = priority)
 
 
