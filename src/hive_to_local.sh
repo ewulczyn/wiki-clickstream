a64bb95db812e7117cd374e0c77e86fd4057e962
ewulczyn <ellerywulczyn@gmail.com>
2017-02-23 15:03:58 -0800

    final commit before wmf departure

--- src/hive_to_local.sh
+++ src/hive_to_local.sh
@@ -1,5 +1,5 @@
 
-table=2016_08_en_unresolved
+table=2017_01_en
 AGG_TABLE=${table}
 FUNNY_NULL_TSV=${table}_clickstream_funny_null.tsv
 FINAL_TSV=${table}_clickstream.tsv
@@ -15,9 +15,9 @@ rm ${FINAL_TSV}.gz
 
 hadoop fs -copyToLocal /user/hive/warehouse/clickstream.db/${AGG_TABLE} ${AGG_TABLE}
 cat header.txt ${AGG_TABLE}/* > ${FUNNY_NULL_TSV}
+#cat header_unresolved.txt ${AGG_TABLE}/* > ${FUNNY_NULL_TSV}
 sed 's/\\N//g' ${FUNNY_NULL_TSV} > ${FINAL_TSV}
 gzip ${FINAL_TSV}
-
 rm -r ${AGG_TABLE}
 rm ${FUNNY_NULL_TSV}
 
@@ -28,3 +28,8 @@ rm ${FINAL_TSV}
 rm ${FINAL_TSV}.gz
 
 scp stat1002.eqiad.wmnet:clickstream/data/${FINAL_TSV}.gz /Users/ellerywulczyn/clickstream/data/${FINAL_TSV}.gz
+
+
+gzcat ${FINAL_TSV}.gz > ${FINAL_TSV}
+head ${FINAL_TSV}
+
